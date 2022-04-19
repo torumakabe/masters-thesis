9451e83055e8ce34952c13cc8dbc171a9079b4fd
ToruMakabe <tomakabe@microsoft.com>
2022-04-19 10:23:56 +0900

    init

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+curl https://raw.githubusercontent.com/ToruMakabe/dotfiles/master/files/gitconfig > $HOME/.gitconfig
---
+++ README.md
@@ -0,0 +1,5 @@
+# 修士論文
+
+## Special thanks to
+
+> [爆速で日本語LaTeX執筆環境を用意する](https://korosuke613.hatenablog.com/entry/2019/06/24/171246)
